dc0b921096b6c160c68ea36bdaf26da90eac80ec
Michael-Chacon <michaelchacon02@gmail.com>
2023-01-23 20:30:11 -0500

    Creé el controlador CategoryController y sus métodos API, dí funcionalidad a dichos métodos y ademas creé el scope para hacer validaciones y constultas con las relaciones del modelo

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -2,15 +2,41 @@
 
 namespace App\Models;
 
+use Illuminate\Contracts\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model
 {
+    protected $fillable = ['name', 'slug'];
+
+    protected $allowInclude = ['posts', 'posts.user'];
+
     use HasFactory;
 
     public function posts()
     {
         return $this->hasMany(Post::class);
+    }
+
+    // $query hace referencia a la consulta que estamos haciendo en el controlador--- Category::included()->findOrFail($id);
+    public function scopeIncluded(Builder $query)
+    {
+        if(empty($this->allowInclude) ||empty(request('included'))){
+            return;
+        }
+        // request es un helper que trae los parametros que viene por la url
+        $relations = explode(',', request('included')); //['post', 'relationship2']
+        $allowInclude = collect($this->allowInclude);
+
+        foreach($relations as $key => $relationship)
+        {
+            if(!$allowInclude->contains($relationship)){
+                unset($relations[$key]);
+            }
+        }
+
+
+        $query->with($relations);
     }
 }
